385db0e75ddfaa074ebd7ce940464be21c0c57ec
Mike Lukashev <boyskyfall@vk.com>
2023-10-31 00:06:01 +0300

    feat: add logstash input plugin code

---
+++ src/main/java/org/logstashplugins/input/util/MessageProcessor.java
@@ -0,0 +1,11 @@
+package org.logstashplugins.input.util;
+
+import tech.ydb.topic.read.Message;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+@FunctionalInterface
+public interface MessageProcessor {
+    Map<String, Object> process(Message message, Consumer<Map<String, Object>> consumer);
+}
